86281b5ba5e9f217d589f000471b1dad0a5f658e
Jovan <jovanwang2002@gmail.com>
2024-06-07 20:22:55 +0800

    i make new reformat pls see if can

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hi</h1>
+  <h2>Bye</h2>
+  <NavBar />
+</template>
+
+<script>
+  import NavBar from '@/components/NavBar.vue';
+
+  export default {
+    name: 'home',
+    
+    components: {
+      NavBar
+    },
+  };
+</script>
