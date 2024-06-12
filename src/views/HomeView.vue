eec8a63dc719aa8b5832ae9f015f3ec8dde9f55b
Tay Yee Teng <tayyeeteng@yahoo.com.sg>
2024-06-12 15:33:47 +0800

    Completed Nav Bar
    - fixed the navigation bar with proper vue implementation
    - merged the central.css with main.css
    - included bootstrap.css and bootstrap.js which is needed for the nav bar to work properly. (unsure about the paths for now so..i just dump it under router for js and assets folder for css)
    - included the title and icon for default every page (can be changed under index.html head)
    
    Error:
    - unable to do the toggling of login/profile script? idk why when i add the script the whole nav bar disappears. (i commented it out under <script> in navbar.vue)

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavBar />
+</template>
+
+<script>
+  import NavBar from '@/components/NavBar.vue';
+
+  export default {
+    name: 'home',
+    
+    components: {
+      NavBar
+    },
+  };
+</script>
