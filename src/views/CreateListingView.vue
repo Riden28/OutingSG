23c9ff4eecef99fabb907d4c3a1dedf4f744a25e
Jovan <jovanwang2002@gmail.com>
2024-06-28 17:57:30 +0800

    created some holder divs for the view
    created left & right, and rows for lower. sorry no time did not do much

--- src/views/CreateListingView.vue
+++ src/views/CreateListingView.vue
@@ -1,6 +1,33 @@
 <template> 
     <NavBar /> 
-    
+    <form action="">
+      <div class="upper">
+
+      </div>
+      <div class="lower">
+        <div class="left">
+
+        </div>
+        <div class="right">
+          <div class="row1">
+            <div class="category">
+              <select name="category" id="">
+                <option value="F&B">F&B</option>
+                <option value="nature">nature</option>
+              </select>
+            </div>
+            <div class="price"></div>
+            <div class="recommended-pax"></div>
+          </div>
+          <div class="row2">
+            <div class="area"></div>
+            <div class="location"></div>
+          </div>
+        </div>
+    </div>
+    </form>
+
+
     <OutingSGFooter/>
 </template> 
  
@@ -17,3 +44,7 @@
     }, 
   }; 
 </script>
+
+<style scoped>
+
+</style>
