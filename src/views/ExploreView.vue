b489ece77cf69a836f1e386c22c5492e73069f24
Tay Yee Teng <tayyeeteng@yahoo.com.sg>
2024-07-12 21:42:15 +0800

    fixed explore page for search bar to work

--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -47,6 +47,63 @@
         <div class="main">
 
             <h2 class="title">Outings</h2>
+
+            <v-container fluid class="mx-auto">
+        <v-infinite-scroll
+            height="700"
+            color=var(--secondary)
+            mode="manual"
+            @load="load"
+            empty-text="No more recommended listings"
+        >
+                <v-row align="start" justify="center">
+                    <v-col  v-for="listing in listings" cols="auto">
+                        <v-card
+                        class="mx-1"
+                        height="280"
+                        width="417"
+                        rounded="xl"
+                        :href="listing.listingPage">
+                        <v-img
+                        :src="listing.url"
+                        height="174px"
+                        cover
+                        ></v-img>
+                        <v-btn
+                            icon="mdi-bookmark-outline"
+                            base-color="transparent"
+                            variant="plain"
+                            @click.prevent="console.log('hi');"
+                        > 
+                            <v-icon
+                                icon="mdi-bookmark"
+                                size="50"
+                                color="white"
+                
+                            ></v-icon>
+                        </v-btn>
+                        
+                                <v-card-title>
+                                    {{ listing.name }}
+                                </v-card-title>
+                            
+                                <v-card-subtitle>
+                                    {{ listing.details }}
+                                </v-card-subtitle>
+
+                                <v-card-title class="price">
+                                    {{ listing.price }}
+                                </v-card-title>
+                            
+                            <!-- replace console.log with function to save outing -->
+                            
+                        </v-card>
+                        
+                    </v-col>
+                
+                </v-row>
+        </v-infinite-scroll>
+        </v-container>
         </div> 
     </div> 
 
@@ -57,6 +114,49 @@
 <script>
 import NavBar from '@/components/NavBar.vue';
 import OutingSGFooter from '@/components/Footer.vue';
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore, collection, doc, setDoc, getDoc, updateDoc, deleteDoc, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import firebaseConfig from './../../firebase/firebaseConfig.js';
+import shuffle from "./../../firebase/firebaseAuthServices.js"
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+// Initialize Firebase Storage
+const storage = getStorage(app);
+
+const querySnapshot = await getDocs(collection(db, "outings"));
+var outings = [];
+querySnapshot.forEach((doc) => {
+    var outing_details = doc.data();
+    
+    // Determine the price based on max_price
+    let price;
+    if (outing_details.max_price === 0) {
+        price = "Free";
+    } else {
+        price = "$" + outing_details.min_price + ' ~ $' + outing_details.max_price;
+    }
+
+    outings.push({
+        listingID: doc.id,
+        name: outing_details.name,
+        details: outing_details.location,
+        price: price,
+        url: outing_details.images.length > 0 ? outing_details.images[0] : null
+    });
+});
+
+outings = shuffle(outings);
 
 export default {
     name: 'explore',
@@ -89,15 +189,28 @@ export default {
             
             querySnapshot.forEach((doc) => {
                 var outing_details = doc.data();
+                
                 var outing_name = outing_details.name.toLowerCase()
                 var search_name = this.searchField.toLowerCase()
+
+                let price;
+                if (outing_details.max_price === 0) {
+                    price = "Free";
+                } else {
+                    price = "$" + outing_details.min_price + ' ~ $' + outing_details.max_price;
+                }
                 
                 if(outing_name.includes(search_name)){
-                    this.listings.push({name: outing_details.name, details: outing_details.description, url: outing_details.images[0]})
+                    this.listings.push({
+                        listingID: doc.id,
+                        name: outing_details.name,
+                        details: outing_details.location,
+                        price: price,
+                        url: outing_details.images.length > 0 ? outing_details.images[0] : null
+                    });                
                 }
             });
             
-            console.log(this.listings);
         }
     },
     created() {
@@ -114,6 +227,27 @@ export default {
 </script>
 
 <style scoped>
+
+.v-btn {
+  position: absolute;
+  top: -6px;
+  right: 8px;
+  /* font-size: 20px; */
+}
+
+.price {
+    position: absolute;
+    right: 0px;
+    bottom: 0px;
+    font-weight: normal;
+}
+
+.location{
+    position: absolute;
+    left: 0px;
+    bottom: 0px;
+    font-weight: normal;
+}
 
 label {
     padding-left: 5px;
