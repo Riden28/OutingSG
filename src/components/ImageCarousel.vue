e1271f2f6dfbbb1bf3dc7ca4d1b9e6260b0500ea
Li Quan <liquan2808@gmail.com>
2024-07-21 00:52:34 +0800

    Reapply "fits images more appropriately"
    This reverts commit 67b7a91f4f179a9048e8b0a43a8b994407660f44.

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="v-carousel">
     <v-carousel
-      show-arrows="hover" 
+      show-arrows="hover"
       cycle
-      :height=carousel_height
+      :height="carousel_height"
       hide-delimiter-background>
       <template v-for="(slide, index) in slides" :key="index">
         <router-link v-if="isHomepage" :to="'/listing/' + slide.listingID" class="carousel-link">
           <v-carousel-item class="clickable">
-            <img v-lazy="slide.url" :alt="'Slide ' + index" width="100%" />
+            <img class="carousel-image" v-lazy="slide.url" :alt="'Slide ' + index" />
           </v-carousel-item>
         </router-link>
         <div v-else class="carousel-link">
           <v-carousel-item>
-            <img v-lazy="slide.url" :alt="'Slide ' + index" width="100%" />
+            <img class="carousel-image" v-lazy="slide.url" :alt="'Slide ' + index" />
           </v-carousel-item>
         </div>
       </template>
-      <div class="carousel_title" v-if="isHomepage"><h3><b>Trending activities</b></h3></div>
+      <div class="carousel_title" v-if="isHomepage">
+        <h3><b>Trending activities</b></h3>
+      </div>
     </v-carousel>
   </div>
 </template>
@@ -34,7 +36,7 @@ export default {
       return this.$route.path === '/';
     }
   },
-  data () {
+  data() {
     return {}
   }
 }
@@ -45,6 +47,7 @@ export default {
   position: relative;
   padding: 0px;
 }
+
 .carousel_title {
   position: absolute;
   text-align: center;
@@ -54,16 +57,24 @@ export default {
   display: flex;
   justify-content: center;
   text-shadow: 
-  -1px -1px 0 black,  
-   1px -1px 0 black,
-  -1px  1px 0 black,
-   1px  1px 0 black; /* black outline */
+    -1px -1px 0 black,  
+     1px -1px 0 black,
+    -1px  1px 0 black,
+     1px  1px 0 black; /* black outline */
 }
+
 .carousel-link {
   display: block;
   width: 100%;
   height: 100%;
 }
+
+.carousel-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* Ensures the image covers the entire carousel item */
+}
+
 .clickable:hover {
   opacity: 0.8; /* Optional: Add hover effect */
   cursor: pointer;
